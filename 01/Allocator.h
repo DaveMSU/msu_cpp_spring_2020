a6771176c09b108c31404dce5f2a0a6b30f02b62
DaveMSU <50180326+DaveMSU@users.noreply.github.com>
2020-03-11 15:29:56 +0300

    Add files via upload

--- 01/Allocator.h
+++ 01/Allocator.h
@@ -1,10 +1,6 @@
 #ifndef ALLOC
 #define ALLOC
 
-extern char* main_ptr;
-extern char* cur_ptr;
-extern char* end_ptr;
-
 void makeAllocator(size_t maxSize);
 void reset();
 char* alloc(size_t size);
