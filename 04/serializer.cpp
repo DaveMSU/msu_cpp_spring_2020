e61501d0d52ea47ce419b8c2d8ff1a1a88582313
DaveMSU <50180326+DaveMSU@users.noreply.github.com>
2020-03-29 16:41:51 +0500

    Add files via upload

---
+++ 04/serializer.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include "serializer.h"
+
+// Сериализатор:
+//Serializer::Serializer( std::ostream& out ) : out_(out){} // Определеяем куда мы пишем(сохраняем).
+
+template <class T>
+Error Serializer::save( T& object ){		// Работаем как с функтором.
+
+	return object.serialize(*this);
+}
+
+template <class... ArgsT>
+Error Serializer::operator()( ArgsT... args ){	
+ 
+	return process(args...);
+}
+
+template <class T, class... ArgsT>
+Error Serializer::process( T t, ArgsT... args ){
+    	
+	one_to_stream(t);
+	return process(args...);
+}
+
+template <class T>
+Error Serializer::process( T t ){
+
+	return one_to_stream(t);
+}  
+
+Error Serializer::one_to_stream( bool var ) const {
+		
+	if( var )
+		out_ << "true" << separator;
+	else
+		out_ << "false" << separator;
+	return Error::NoError;
+}
+
+Error Serializer::one_to_stream( uint64_t var ) const {
+
+	while( var ){ // Попробовать без while!
+
+		out_ << char(var % 10 + '0');
+		var /= 10;
+	}
+
+	return Error::NoError;
+}
+
+// Десериализатор:
+Deserializer::Deserializer( std::ifstream& in ) : in_(in){}
+
+template <class T>
+Error Deserializer::load( T& object ){
+
+	if( one_from_stream(t) == Error::CorruptedArchive )
+            return Error::CorruptedArchive;
+	return object.Deserializer(*this);
+}
+
+template <class... ArgsT>
+Error Deserializer::operator( ArgsT... args ){
+
+
+
+       	return process(args...);
+}
+
+template <class T>
+Error Deserializer::process( T& t ){
+
+	return one_from_stream(t);
+}
+
+template<class T>
+Error Deserializer::one_from_stream( T& t ){
+
+        return Error::CorruptedArchive;
+}
+
+Error Deserializer::one_from_stream( bool& var ){
+
+        std::string text;
+        in_ >> text;
+
+    	if (text == "true") {
+
+		var = true;
+                return Error::NoError;
+	}
+        else if (text == "false") {
+
+                value = false;
+                return Error::NoError;
+	}
+        return Error::CorruptedArchive;
+}
+
+Error Deserializer::one_from_stream( uint64_t& var ){
+                
+	       	std::string text;
+        	in_ >> text;
+        
+		if (!isdigit(text[0])) {
+
+			return Error::CorruptedArchive;
+		}
+        
+		try {
+            		
+			var = std::stoull(text);
+            		return Error::NoError;
+        	} 
+		
+		catch (std::exception &x) {
+			
+			return Error::CorruptedArchive;
+	        }
+}
---
+++ 04/serializer.h
@@ -0,0 +1,60 @@
+#pragma onec
+
+#include <fstream>
+#include <iostream>
+
+enum class Error
+{
+        NoError,
+        CorruptedArchive
+};
+
+// Сериализатор:
+class Serializer{
+
+        static constexpr char Separator = ' ';
+        std::ofstream& out_;
+public:
+        explicit Serializer( std::ostream& out ) : out_(out){}
+        template <class T>
+        Error save( T& object );
+
+        template <class... ArgsT>
+        Error operator()( ArgsT... args );
+
+private:
+        // process использует variadic templates
+        template<class T, class... ArgsT>
+        Error process( T t, ArgsT... args );
+
+        template<class T>
+        Error process( T t );
+
+        Error one_to_stream( bool     var ) const;
+        Error one_to_stream( uint64_t var ) const;
+};
+
+
+// Десериализатор:
+class Deserializer{
+
+	std::ifstream &in_;
+public:
+	explicit Deserializer( std::istream& in );
+
+	template<class T>
+	Error load( T& object );
+private:
+	template <class... ArgsT>
+	Error operator()( ArgsT... args );
+
+	template <class T>
+	Error process( T& t );
+
+	template<class T>
+	Error one_from_stream( T& t );
+
+	Error one_from_stream(bool& var);
+	Error one_from_stream( uint64_t& var );
+};
+
