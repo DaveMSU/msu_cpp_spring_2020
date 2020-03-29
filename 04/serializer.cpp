60b354b6020b03d7dd08fee86bd4aa430f34fad9
DaveMSU <50180326+DaveMSU@users.noreply.github.com>
2020-03-29 18:21:22 +0500

    Add files via upload

--- 04/serializer.cpp
+++ 04/serializer.cpp
@@ -1,108 +1,124 @@
 #include <iostream>
-#include "serializer.h"
+
+enum class Error{
+
+	NoError,
+	CorruptedArchive
+};
 
 // Сериализатор:
-//Serializer::Serializer( std::ostream& out ) : out_(out){} // Определеяем куда мы пишем(сохраняем).
+class Serializer{
 
-template <class T>
-Error Serializer::save( T& object ){		// Работаем как с функтором.
-
-	return object.serialize(*this);
-}
-
-template <class... ArgsT>
-Error Serializer::operator()( ArgsT... args ){	
- 
-	return process(args...);
-}
-
-template <class T, class... ArgsT>
-Error Serializer::process( T t, ArgsT... args ){
-    	
-	one_to_stream(t);
-	return process(args...);
-}
-
-template <class T>
-Error Serializer::process( T t ){
-
-	return one_to_stream(t);
-}  
-
-Error Serializer::one_to_stream( bool var ) const {
+	static constexpr char Separator = ' ';
+	std::ostream& out_;
+public:
+	explicit Serializer( std::ostream& out ) : out_(out){}
+	template <class T>
+	Error save( T& object ){		
 		
-	if( var )
-		out_ << "true" << Separator;
-	else
-		out_ << "false" << Separator;
-	return Error::NoError;
-}
-
-Error Serializer::one_to_stream( uint64_t var ) const {
-
-	while( var ){ // Попробовать без while!
-
-		out_ << char(var % 10 + '0');
-		var /= 10;
+		return object.serialize(*this);
+	}
+	
+	template <class... ArgsT>
+	Error operator()( ArgsT... args ){	
+		
+		return process(args...);
 	}
 
-	return Error::NoError;
-}
+	template <class T, class... ArgsT>
+	Error process( T t, ArgsT... args ){
+		
+		one_to_stream(t);
+		return process(args...);
+	}
+
+	template <class T>
+	Error process( T t ){
+
+		return one_to_stream(t);
+	}
+
+	Error one_to_stream( bool var ) const {
+		
+		if( var )
+			out_ << "true" << Separator;
+		else
+			out_ << "false" << Separator;
+		return Error::NoError;
+	}
+
+	Error one_to_stream( uint64_t var ) const {
+
+		while( var ){ // Попробовать без while!
+
+			out_ << char(var % 10 + '0');
+			var /= 10;
+		}
+
+		return Error::NoError;
+	}
+};
 
 // Десериализатор:
-//explicit Deserializer::Deserializer( std::ifstream& in ) : in_(in){}
+class Deserializer{
 
-template <class T>
-Error Deserializer::load( T& object ){
+	std::istream& in_;
+public:
+	explicit Deserializer( std::istream& in ) : in_(in){}
 
-	return object.Deserializer(*this);
-}
+	template <class T>
+	Error load( T& object ){
 
-template <class... ArgsT>
-Error Deserializer::operator()( ArgsT&... args ){
-
-       	return process(args...);
-}
-
-template <class T, class... ArgsT>
-Error process(T& t, ArgsT&... args){
-
-        if( realize(t) == Error::CorruptedArchive )
-            return Error::CorruptedArchive;
-        return process(args...);
-}
-
-template <class T>
-Error Deserializer::process( T& t ){
-
-	return one_from_stream(t);
-}
-
-template<class T>
-Error Deserializer::one_from_stream( T& t ){
-
-        return Error::CorruptedArchive;
-}
-
-Error Deserializer::one_from_stream( bool& var ){
-
-        std::string text;
-        in_ >> text;
-
-    	if (text == "true") {
-
-		var = true;
-                return Error::NoError;
+		return object.deserialize(*this);
 	}
-        else if (text == "false") {
 
-                var = false;
-                return Error::NoError;
+	template <class... ArgsT>
+	Error operator()( ArgsT&... args ){
+
+		return process(args...);
 	}
-        return Error::CorruptedArchive;
-}
 
-Error Deserializer::one_from_stream( uint64_t& var ){
+	template <class T, class... ArgsT>
+	Error process(T& t, ArgsT&... args){
+
+		if( one_from_stream(t) == Error::CorruptedArchive )
+			return Error::CorruptedArchive;
+		return process(args...);
+	}
+
+	template <class T>
+	Error process( T& t ){
+		
+		return one_from_stream(t);
+	}
+
+	template<class T>
+	Error one_from_stream( T& t ){
+		
+		return Error::CorruptedArchive;
+	}
+
+	Error one_from_stream( bool& var ){
+
+		std::string text;
+		in_ >> text;
+
+	   	if (text == "true") {
+
+			var = true;
+			return Error::NoError;
+
+		}
+		else if (text == "false") {
+
+			var = false;
+			return Error::NoError;
+		}
+		
+		return Error::CorruptedArchive;
+	}
+
+	Error one_from_stream( uint64_t& var ){
                 
 	       	std::string text;
         	in_ >> text;
@@ -122,4 +138,5 @@ Error Deserializer::one_from_stream( uint64_t& var ){
 			
 			return Error::CorruptedArchive;
 	        }
-}
+	}
+};
--- 04/test.cpp
+++ 04/test.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 #include <sstream>
+#include <cassert>
 #include <cctype>
-#include "Serializer.h"
-
-#define checkEqual(x, y) do { if ((x) != (y)) { std::cout << "at line " << __LINE__ << ": " << (x) << " != " << (y) << '\n'; }; } while(0)
-#define checkTrue(cond) do { if (!(cond)) std::cout << "at line " << __LINE__ << ": " << #cond << '\n'; } while(0)
+#include "serializer.cpp"
 
 struct Data
 {
@@ -13,59 +11,36 @@ struct Data
     uint64_t c;
 
     template <class Serializer>
-    Error serialize(Serializer& source)
-    {
+    Error serialize(Serializer& source){
+
         return source(a, b, c);
     }
-    Error deserialize(Deserializer& source)
-    {
+
+    Error deserialize(Deserializer& source){
+
         return source(a, b, c);
     }
 };
 
-int main()
-{
-    Data x { 1, true, 2 };
+int main(){
 
-    std::stringstream s1;
+	Data x { 1, true, 2 };
 
-    Serializer serializer(s1);
-    checkTrue(serializer.save(x) == Error::NoError);
+	std::stringstream stream;
 
-    Data y { 0, false, 0 };
+	Serializer serializer(stream);
+	serializer.save(x);
 
-    Deserializer d1(s1);
-    checkTrue(d1.load(y) == Error::NoError);
+	Data y { 0, false, 0 };
 
-    checkEqual(x.a, y.a);
-    checkEqual(x.b, y.b);
-    checkEqual(x.c, y.c);
+	Deserializer deserializer(stream);
+	const Error err = deserializer.load(y);
 
-    auto s2 = std::stringstream("");
-    Deserializer d2(s2);
-    checkTrue(d2.load(y) == Error::CorruptedArchive);
+	assert(err == Error::NoError);
 
-    auto s3 = std::stringstream("1 2 3");
-    Deserializer d3(s3);
-    checkTrue(d3.load(y) == Error::CorruptedArchive);
+	assert(x.a == y.a);
+	assert(x.b == y.b);
+	assert(x.c == y.c);
 
-    auto s4 = std::stringstream("1 true -3");
-    Deserializer d4(s4);
-    checkTrue(d4.load(y) == Error::CorruptedArchive);
-
-    auto s5 = std::stringstream("false 1");
-    Deserializer d5(s5);
-    checkTrue(d5.load(y) == Error::CorruptedArchive);
-
-    y = { 0, true, 0 };
-
-    auto s6 = std::stringstream("100 false 500");
-    Deserializer d6(s6);
-    checkTrue(d6.load(y) == Error::NoError);
-
-    checkEqual(y.a, 100);
-    checkEqual(y.b, false);
-    checkEqual(y.c, 500);
-
-    return 0;
-} 
+	return 0;
+}
