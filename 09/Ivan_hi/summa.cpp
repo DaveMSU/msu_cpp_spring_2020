8c911895cf8b87b0199676c515bd9a75686620ec
DaveMSU <50180326+DaveMSU@users.noreply.github.com>
2020-05-14 20:37:25 +0500

    Add files via upload

--- 09/Ivan_hi/summa.cpp
+++ 09/Ivan_hi/summa.cpp
@@ -1,1 +1,41 @@
-a
+#include <iostream>
+#include <cctype>      
+#include <fstream>
+#include <cstdlib>
+#include <thread>
+
+int main(){
+
+	std::ifstream fin("numbers");
+
+	int num;
+	int sum = 0;
+	char c = 'A';
+	std::string buf;
+
+	while( !fin.eof() ){
+
+		buf = "";
+
+		while( c != '\n' ){
+
+			fin >> c;
+			std::cout << c;
+			if( isspace(c) ){
+			
+				num = atoi(buf.c_str());
+				buf = "";
+				sum += num;
+				std::cout << sum << " " << num;
+			}
+			else
+				buf += c;
+		}
+	}
+
+	std::cout << sum;
+
+
+	
+	return 0;
+}
